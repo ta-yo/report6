d44d696f4ab1bb1805d0c2c9fc9939ccc8161fc2
yoshiki tanahara <e165753@ie.u-ryukyu.ac.jp>
2017-01-17 00:27:41 +0900

    レポート４->レポート６

---
+++ src/main/jp/ac/uryukyu/ie/e165753/Enemy.java
@@ -0,0 +1,7 @@
+package jp.ac.uryukyu.ie.e165753;
+
+public class Enemy extends LivingThing{
+    public Enemy(String name, int hitPoint, int attack){
+        super(name,hitPoint,attack);
+    }
+}
